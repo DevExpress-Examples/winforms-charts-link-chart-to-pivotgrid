8d2fa6d2ede6ca0191c4d0c5886b51f7a8bffa73
DevExpressExampleBot <fakeEmail>
2021-09-13 16:02:33 +0300

    autogenerate VB

--- VB/Pivot_Chart_1/Form1.Designer.vb
+++ VB/Pivot_Chart_1/Form1.Designer.vb
@@ -1,6 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Namespace Pivot_Chart_1
+﻿Namespace Pivot_Chart
 	Partial Public Class Form1
 		''' <summary>
 		''' Required designer variable.
@@ -25,124 +23,127 @@ Namespace Pivot_Chart_1
 		''' the contents of this method with the code editor.
 		''' </summary>
 		Private Sub InitializeComponent()
-            Me.components = New System.ComponentModel.Container()
-            Dim XyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
-            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-            Me.gSPBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-            Me.gspDataSet = New Pivot_Chart_1.gspDataSet()
-            Me.fieldID1 = New DevExpress.XtraPivotGrid.PivotGridField()
-            Me.fieldRegion1 = New DevExpress.XtraPivotGrid.PivotGridField()
-            Me.fieldYear1 = New DevExpress.XtraPivotGrid.PivotGridField()
-            Me.fieldGSP1 = New DevExpress.XtraPivotGrid.PivotGridField()
-            Me.gSPTableAdapter = New Pivot_Chart_1.gspDataSetTableAdapters.GSPTableAdapter()
-            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gSPBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gspDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-            Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
-            Me.pivotGridControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-                Or System.Windows.Forms.AnchorStyles.Left) _
-                Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-            Me.pivotGridControl1.DataSource = Me.gSPBindingSource
-            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldID1, Me.fieldRegion1, Me.fieldYear1, Me.fieldGSP1})
-            Me.pivotGridControl1.Location = New System.Drawing.Point(12, 241)
-            Me.pivotGridControl1.Name = "pivotGridControl1"
-            Me.pivotGridControl1.OptionsChartDataSource.FieldValuesProvideMode = DevExpress.XtraPivotGrid.PivotChartFieldValuesProvideMode.DisplayText
-            Me.pivotGridControl1.Size = New System.Drawing.Size(787, 351)
-            Me.pivotGridControl1.TabIndex = 0
-            '
-            'gSPBindingSource
-            '
-            Me.gSPBindingSource.DataMember = "GSP"
-            Me.gSPBindingSource.DataSource = Me.gspDataSet
-            '
-            'gspDataSet
-            '
-            Me.gspDataSet.DataSetName = "gspDataSet"
-            Me.gspDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-            '
-            'fieldID1
-            '
-            Me.fieldID1.AreaIndex = 0
-            Me.fieldID1.Caption = "ID"
-            Me.fieldID1.FieldName = "ID"
-            Me.fieldID1.Name = "fieldID1"
-            '
-            'fieldRegion1
-            '
-            Me.fieldRegion1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-            Me.fieldRegion1.AreaIndex = 0
-            Me.fieldRegion1.Caption = "Region"
-            Me.fieldRegion1.FieldName = "Region"
-            Me.fieldRegion1.Name = "fieldRegion1"
-            '
-            'fieldYear1
-            '
-            Me.fieldYear1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-            Me.fieldYear1.AreaIndex = 0
-            Me.fieldYear1.Caption = "Year"
-            Me.fieldYear1.FieldName = "Year"
-            Me.fieldYear1.Name = "fieldYear1"
-            '
-            'fieldGSP1
-            '
-            Me.fieldGSP1.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-            Me.fieldGSP1.AreaIndex = 0
-            Me.fieldGSP1.Caption = "GSP"
-            Me.fieldGSP1.FieldName = "GSP"
-            Me.fieldGSP1.Name = "fieldGSP1"
-            '
-            'gSPTableAdapter
-            '
-            Me.gSPTableAdapter.ClearBeforeFill = True
-            '
-            'chartControl1
-            '
-            Me.chartControl1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-            Me.chartControl1.DataSource = Me.pivotGridControl1
-            XyDiagram1.AxisX.Label.Staggered = True
-            XyDiagram1.AxisX.Title.Text = "Year"
-            XyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
-            XyDiagram1.AxisX.WholeRange.AutoSideMargins = True
-            XyDiagram1.AxisY.Title.Text = "GSP"
-            XyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-            XyDiagram1.AxisY.WholeRange.AutoSideMargins = True
-            Me.chartControl1.Diagram = XyDiagram1
-            Me.chartControl1.Legend.MaxHorizontalPercentage = 30.0R
-            Me.chartControl1.Location = New System.Drawing.Point(13, 13)
-            Me.chartControl1.Name = "chartControl1"
-            Me.chartControl1.SeriesDataMember = "Series"
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
-            Me.chartControl1.SeriesTemplate.ArgumentDataMember = "Arguments"
-            Me.chartControl1.SeriesTemplate.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.Qualitative
-            Me.chartControl1.SeriesTemplate.LabelsVisibility = DevExpress.Utils.DefaultBoolean.[False]
-            Me.chartControl1.SeriesTemplate.ValueDataMembersSerializable = "Values"
-            Me.chartControl1.Size = New System.Drawing.Size(786, 217)
-            Me.chartControl1.TabIndex = 1
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(811, 604)
-            Me.Controls.Add(Me.chartControl1)
-            Me.Controls.Add(Me.pivotGridControl1)
-            Me.Name = "Form1"
-            Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gSPBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gspDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            Me.ResumeLayout(False)
+			Me.components = New System.ComponentModel.Container()
+			Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+			Me.gSPBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+			Me.gspDataSet = New Pivot_Chart.gspDataSet()
+			Me.fieldID1 = New DevExpress.XtraPivotGrid.PivotGridField()
+			Me.fieldRegion1 = New DevExpress.XtraPivotGrid.PivotGridField()
+			Me.fieldYear1 = New DevExpress.XtraPivotGrid.PivotGridField()
+			Me.fieldGSP1 = New DevExpress.XtraPivotGrid.PivotGridField()
+			Me.gSPTableAdapter = New Pivot_Chart.gspDataSetTableAdapters.GSPTableAdapter()
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gSPBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gspDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' pivotGridControl1
+			' 
+			Me.pivotGridControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.pivotGridControl1.DataSource = Me.gSPBindingSource
+			Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldID1, Me.fieldRegion1, Me.fieldYear1, Me.fieldGSP1})
+			Me.pivotGridControl1.Location = New System.Drawing.Point(12, 241)
+			Me.pivotGridControl1.Name = "pivotGridControl1"
+			Me.pivotGridControl1.OptionsChartDataSource.FieldValuesProvideMode = DevExpress.XtraPivotGrid.PivotChartFieldValuesProvideMode.DisplayText
+			Me.pivotGridControl1.Size = New System.Drawing.Size(823, 313)
+			Me.pivotGridControl1.TabIndex = 0
+			' 
+			' gSPBindingSource
+			' 
+			Me.gSPBindingSource.DataMember = "GSP"
+			Me.gSPBindingSource.DataSource = Me.gspDataSet
+			' 
+			' gspDataSet
+			' 
+			Me.gspDataSet.DataSetName = "gspDataSet"
+			Me.gspDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' fieldID1
+			' 
+			Me.fieldID1.AreaIndex = 0
+			Me.fieldID1.Caption = "ID"
+			Me.fieldID1.FieldName = "ID"
+			Me.fieldID1.Name = "fieldID1"
+			' 
+			' fieldRegion1
+			' 
+			Me.fieldRegion1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+			Me.fieldRegion1.AreaIndex = 0
+			Me.fieldRegion1.Caption = "Region"
+			Me.fieldRegion1.FieldName = "Region"
+			Me.fieldRegion1.Name = "fieldRegion1"
+			' 
+			' fieldYear1
+			' 
+			Me.fieldYear1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+			Me.fieldYear1.AreaIndex = 0
+			Me.fieldYear1.Caption = "Year"
+			Me.fieldYear1.FieldName = "Year"
+			Me.fieldYear1.Name = "fieldYear1"
+			' 
+			' fieldGSP1
+			' 
+			Me.fieldGSP1.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+			Me.fieldGSP1.AreaIndex = 0
+			Me.fieldGSP1.Caption = "GSP"
+			Me.fieldGSP1.FieldName = "GSP"
+			Me.fieldGSP1.Name = "fieldGSP1"
+			' 
+			' gSPTableAdapter
+			' 
+			Me.gSPTableAdapter.ClearBeforeFill = True
+			' 
+			' chartControl1
+			' 
+			Me.chartControl1.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.chartControl1.DataSource = Me.pivotGridControl1
+			xyDiagram1.AxisX.Label.Staggered = True
+			xyDiagram1.AxisX.Title.Text = "Year"
+			xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+			xyDiagram1.AxisX.WholeRange.AutoSideMargins = True
+			xyDiagram1.AxisY.Title.Text = "GSP"
+			xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+			xyDiagram1.AxisY.WholeRange.AutoSideMargins = True
+			Me.chartControl1.Diagram = xyDiagram1
+			Me.chartControl1.Legend.MaxHorizontalPercentage = 30R
+			Me.chartControl1.Location = New System.Drawing.Point(13, 13)
+			Me.chartControl1.Name = "chartControl1"
+			Me.chartControl1.SeriesDataMember = "Series"
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+			Me.chartControl1.SeriesTemplate.ArgumentDataMember = "Arguments"
+			Me.chartControl1.SeriesTemplate.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.Qualitative
+			Me.chartControl1.SeriesTemplate.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False
+			Me.chartControl1.SeriesTemplate.ValueDataMembersSerializable = "Values"
+			Me.chartControl1.Size = New System.Drawing.Size(822, 217)
+			Me.chartControl1.TabIndex = 1
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.chartControl1.BoundDataChanged += new DevExpress.XtraCharts.BoundDataChangedEventHandler(this.chartControl1_BoundDataChanged);
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.chartControl1.CustomizeXAxisLabels += new DevExpress.XtraCharts.CustomizeXAxisLabelsEventHandler(this.chartControl1_CustomizeXAxisLabels);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(847, 566)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Controls.Add(Me.pivotGridControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
+			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gSPBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gspDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
 
-        End Sub
+		End Sub
 
 		#End Region
 
--- VB/Pivot_Chart_1/Form1.vb
+++ VB/Pivot_Chart_1/Form1.vb
@@ -1,17 +1,17 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
 ' ...
 
-Namespace Pivot_Chart_1
+Namespace Pivot_Chart
 	Partial Public Class Form1
 		Inherits Form
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 			' TODO: This line of code loads data into the 'gspDataSet.GSP' table. You can move, or remove it, as needed.
 			Me.gSPTableAdapter.Fill(Me.gspDataSet.GSP)
 		End Sub
@@ -23,7 +23,7 @@ Namespace Pivot_Chart_1
 
 		' Change the series view type to Line.
 		Private Sub chartControl1_BoundDataChanged(ByVal sender As Object, ByVal e As EventArgs) Handles chartControl1.BoundDataChanged
-			CType(sender, ChartControl).SeriesTemplate.ChangeView(ViewType.Line)
+			DirectCast(sender, ChartControl).SeriesTemplate.ChangeView(ViewType.Line)
 		End Sub
 
 	End Class
